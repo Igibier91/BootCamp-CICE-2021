91c87d7a23c1170b863382030447985032ee42f6
Igibier91 <igiber91@gmail.com>
2021-09-28 17:51:54 +0200

    [wip] Mago

---
+++ Mis Ejercicios/TrucoMagia_Reto/TrucoMagia_Reto/Modules/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrucoMagia_Reto
+//
+//  Created by Ignacio Gilabert Bernal on 28/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
